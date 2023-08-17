ae93c02b6678afd83a38e4785cff27ee48907f25
Tyler <t.woods95@aol.com>
2023-08-17 18:30:46 -0400

    updated handlebars

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,5 +1,6 @@
-// const newFormHandler = async (event) => {
-//     event.preventDefault();
+const newFormHandler = async (event) => {
+    event.preventDefault();
 
-//     const name = document.querySelector('#project-name').value.trim();
-// }
+    const name = document.querySelector('#project-name').value.trim();
+    
+}
