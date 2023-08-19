055309d007db58a75cf1b1dfbb0eef11d8734a23
Tyler <t.woods95@aol.com>
2023-08-19 19:01:49 -0400

    updated post form in profile

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,7 +4,6 @@ const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
     try {
-
         res.render('homepage')
     } catch (err) {
         res.status(500).json(err);
@@ -42,6 +41,16 @@ router.get('/profile', withAuth, async (req, res) => {
     } catch (err) {
       res.status(500).json(err);
     }
+});
+
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/profile');
+    return;
+  }
+
+  res.render('login');
 });
 
 module.exports = router;
